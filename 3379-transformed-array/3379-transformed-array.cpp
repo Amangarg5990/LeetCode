086aa1da8134bef5a0fbf77f58e0fa69497e0367
Aman Garg <152692614+Amangarg5990@users.noreply.github.com>
2026-02-05 11:48:01 +0530

    Time: 3 ms (89.69%), Space: 25.7 MB (70.75%) - LeetHub

--- 3379-transformed-array/3379-transformed-array.cpp
+++ 3379-transformed-array/3379-transformed-array.cpp
@@ -1,3 +1,22 @@
+// class Solution {
+// public:
+//     vector<int> constructTransformedArray(vector<int>& nums) {
+//         int m = nums.size();
+//         vector<int> res(m);
+
+//         for (int i = 0; i < m; i++) {
+//             if (nums[i] == 0) {
+//                 res[i] = nums[i];
+//             } else {
+//                 int idx = (i + nums[i]) % m;
+//                 if (idx < 0) idx += m;
+//                 res[i] = nums[idx];
+//             }
+//         }
+//         return res;
+//     }
+// };
+
 class Solution {
 public:
     vector<int> constructTransformedArray(vector<int>& nums) {
@@ -5,12 +24,16 @@ public:
         vector<int> res(m);
 
         for (int i = 0; i < m; i++) {
-            if (nums[i] == 0) {
+            if (nums[i] > 0) {
+                int k = (i + nums[i] % m) % m;
+                res[i] = nums[k];
+            }
+            else if (nums[i] == 0) {
                 res[i] = nums[i];
-            } else {
-                int idx = (i + nums[i]) % m;
-                if (idx < 0) idx += m;
-                res[i] = nums[idx];
+            }
+            else {
+                int j = (i + (nums[i] % m) + m) % m;
+                res[i] = nums[j];
             }
         }
         return res;
