ac0ce6295a876d81c2bbb88eb2d250b951e05775
Aman Garg <152692614+Amangarg5990@users.noreply.github.com>
2026-02-09 14:59:12 +0530

    Time: 0 ms (100%), Space: 10.5 MB (51.24%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,21 +1,45 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int n=s.size();
-        string ans="";
-        reverse(s.begin(),s.end());
-        for(int i=0;i<n;i++){
-            string word="";
-            while(i<n && s[i]!=' '){
-                word+=s[i];
-                i++;
-            }
-            reverse(word.begin(),word.end());
-            if(word.length()>0){
-                ans+=' '+word;
+        // int n=s.size();
+        // string ans="";
+        // reverse(s.begin(),s.end());
+        // for(int i=0;i<n;i++){
+        //     string word="";
+        //     while(i<n && s[i]!=' '){
+        //         word+=s[i];
+        //         i++;
+        //     }
+        //     reverse(word.begin(),word.end());
+        //     if(word.length()>0){
+        //         ans+=' '+word;
                 
+        //     }
+        // }
+        // return ans.substr(1);
+        int n = s.size();
+    stack<string> st;
+    string a = "";
+
+    for (int i = 0; i < n; i++) {
+        if (s[i] == ' ') {
+            if (!a.empty()) {
+                st.push(a);
+                a = "";
             }
+        } else {
+            a.push_back(s[i]);
         }
-        return ans.substr(1);
+    }
+    if (!a.empty()) {
+        st.push(a);
+    }
+    string ans = "";
+    while (!st.empty()) {
+        ans += st.top();
+        st.pop();
+        if (!st.empty()) ans += " ";
+    }
+    return ans;
     }
 };
