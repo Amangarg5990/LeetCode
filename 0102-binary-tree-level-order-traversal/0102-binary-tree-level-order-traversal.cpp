f74aa12627ca4eadbef73e32fdd567819437ec61
Aman Garg <152692614+Amangarg5990@users.noreply.github.com>
2026-02-16 14:33:33 +0530

    Time: 0 ms (100%), Space: 17.3 MB (16.45%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -12,22 +12,43 @@
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>> v;
-        if(root == NULL) return v;
+        // vector<vector<int>> v;
+        // if(root == NULL) return v;
+        // queue<TreeNode*> q;
+        // q.push(root);
+        // while(!q.empty()){
+        //     int size = q.size();
+        //     vector<int> level;
+        //     for(int i = 0 ; i< size ; i++){
+        //         TreeNode* node = q.front();
+        //         q.pop();
+        //         if(node->left != NULL) q.push(node->left);
+        //         if(node->right != NULL) q.push(node->right);
+        //         level.push_back(node->val);
+        //     }
+        //     v.push_back(level);
+        // }
+        // return v;
+
+
+        vector<vector<int>> result;
+        if(root==NULL){ 
+            return result;
+        }
         queue<TreeNode*> q;
         q.push(root);
         while(!q.empty()){
-            int size = q.size();
-            vector<int> level;
-            for(int i = 0 ; i< size ; i++){
-                TreeNode* node = q.front();
+            vector<int> levelElements;
+            int levelsize=q.size();
+            for(int i=0;i<levelsize;i++){
+                TreeNode*frontNode=q.front();
                 q.pop();
-                if(node->left != NULL) q.push(node->left);
-                if(node->right != NULL) q.push(node->right);
-                level.push_back(node->val);
+                levelElements.push_back(frontNode->val);
+                if(frontNode->left) q.push(frontNode->left);
+                if(frontNode->right) q.push(frontNode->right);
             }
-            v.push_back(level);
+            result.push_back(levelElements);
         }
-        return v;
+        return result;
     }
 };
