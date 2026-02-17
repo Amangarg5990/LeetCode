c848b4b12bb22f309a6bdea32f76bfae82eae061
Aman Garg <152692614+Amangarg5990@users.noreply.github.com>
2026-02-17 11:53:01 +0530

    Time: 0 ms (100%), Space: 64.9 MB (61.45%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
@@ -12,24 +12,34 @@
 class Solution {
 public:
     int rangeSumBST(TreeNode* root, int low, int high) {
-        int sum=0;
-        if(root==NULL){ 
+        // int sum=0;
+        // if(root==NULL){ 
+        //     return 0;
+        // }
+        // queue<TreeNode*> q;
+        // q.push(root);
+        // while(!q.empty()){
+        //     int levelsize=q.size();
+        //     for(int i=0;i<levelsize;i++){
+        //         TreeNode*frontNode=q.front();
+        //         q.pop();
+        //         if(frontNode->val>=low && frontNode->val<=high){
+        //             sum+=frontNode->val;
+        //         }
+        //         if(frontNode->left) q.push(frontNode->left);
+        //         if(frontNode->right) q.push(frontNode->right);
+        //     }
+        // }
+        // return sum;
+        if(root==NULL){
             return 0;
         }
-        queue<TreeNode*> q;
-        q.push(root);
-        while(!q.empty()){
-            int levelsize=q.size();
-            for(int i=0;i<levelsize;i++){
-                TreeNode*frontNode=q.front();
-                q.pop();
-                if(frontNode->val>=low && frontNode->val<=high){
-                    sum+=frontNode->val;
-                }
-                if(frontNode->left) q.push(frontNode->left);
-                if(frontNode->right) q.push(frontNode->right);
-            }
+        int sum=0;
+        if(root->val>=low && root->val<=high){
+            sum+=root->val;
         }
-        return sum;
+        int x=rangeSumBST(root->left,low,high);
+        int y=rangeSumBST(root->right,low,high);
+        return sum+x+y;
     }
 };
