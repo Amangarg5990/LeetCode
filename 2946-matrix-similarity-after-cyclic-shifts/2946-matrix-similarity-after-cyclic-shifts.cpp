4540636e7247e89ed2fa9d72ed5aafc6fa69afc7
Aman Garg <152692614+Amangarg5990@users.noreply.github.com>
2026-03-27 16:29:16 +0530

    Time: 10 ms (5.59%), Space: 32.8 MB (18.63%) - LeetHub

--- 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cpp
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cpp
@@ -3,16 +3,12 @@ public:
     bool areSimilar(vector<vector<int>>& mat, int k) {
         int n = mat.size();
         int m = mat[0].size();
-
         vector<vector<int>> du = mat;
-
-        k = k % m;
+        // k = k % m;
         int p = 0;
-
         while(p < k){
             for(int i = 0; i < n; i++){
                 if(i % 2 == 0){
-                    // left shift
                     int temp = mat[i][0];
                     for(int j = 0; j < m-1; j++){
                         mat[i][j] = mat[i][j+1];
@@ -20,7 +16,6 @@ public:
                     mat[i][m-1] = temp;
                 }
                 else{
-                    // ✅ FIXED right shift
                     int temp2 = mat[i][m-1];
                     for(int j = m-1; j > 0; j--){
                         mat[i][j] = mat[i][j-1];
