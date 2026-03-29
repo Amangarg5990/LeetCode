a49c5ad78fc082453c6b1e892b5d45436bfdbfc9
Aman Garg <152692614+Amangarg5990@users.noreply.github.com>
2026-03-30 03:06:49 +0530

    Time: 3 ms (6.16%), Space: 13 MB (5.46%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -3,9 +3,9 @@ public:
     int helper(vector<int>& dp,int idx,vector<int> he){
         if(idx<0) return 0;
         if(dp[idx]!=-1) return dp[idx];
-        int left=helper(dp,idx-2,he)+he[idx];
-        int right=helper(dp,idx-1,he);
-        return dp[idx]=max(left,right);
+        int safe=helper(dp,idx-2,he)+he[idx];
+        int alarm=helper(dp,idx-1,he);
+        return dp[idx]=max(safe,alarm);
     }
     int rob(vector<int>& nums) {
         int n=nums.size();
