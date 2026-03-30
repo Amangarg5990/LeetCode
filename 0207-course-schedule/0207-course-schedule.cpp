da6d5a554fe0f8fd7662de0e96adc53e709af35d
Aman Garg <152692614+Amangarg5990@users.noreply.github.com>
2026-03-31 03:56:07 +0530

    Time: 8 ms (35.71%), Space: 19.5 MB (48.99%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool detect(vector<vector<int>>& adj,vector<int>& vis, int src, vector<int>& path){
+        vis[src]=1;
+        path[src]=1;
+        for(auto it: adj[src]){
+            if(!vis[it]){
+                if(detect(adj,vis,it,path)) return true;
+            }
+            else if(path[it]){
+                return true;
+            }
+        }
+        path[src]=0;
+        return false;
+        
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> vis(numCourses,0);
+        vector<int> path(numCourses,0);
+        vector<vector<int>> adj(numCourses);
+        for(int i = 0; i < prerequisites.size(); i++){
+            int u = prerequisites[i][0];
+            int v = prerequisites[i][1];
+            adj[v].push_back(u);
+        }
+        for(int i=0;i<numCourses;i++){
+            if(!vis[i]){
+                if(detect(adj,vis,i, path)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
