0edf43cd7c7827d6928ad7cf9a9d56f9df454cc7
Aman Garg <152692614+Amangarg5990@users.noreply.github.com>
2026-04-01 21:50:20 +0530

    Time: 4 ms (64.55%), Space: 19.1 MB (90.97%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,36 +1,35 @@
 class Solution {
 public:
-    bool detect(vector<vector<int>>& adj,vector<int>& vis, int src, vector<int>& path){
-        vis[src]=1;
-        path[src]=1;
-        for(auto it: adj[src]){
-            if(!vis[it]){
-                if(detect(adj,vis,it,path)) return true;
-            }
-            else if(path[it]){
-                return true;
-            }
-        }
-        path[src]=0;
-        return false;
-        
-    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> vis(numCourses,0);
-        vector<int> path(numCourses,0);
         vector<vector<int>> adj(numCourses);
-        for(int i = 0; i < prerequisites.size(); i++){
-            int u = prerequisites[i][0];
-            int v = prerequisites[i][1];
+        vector<int> indegree(numCourses,0);
+        for(int i=0;i<prerequisites.size();i++){
+            int u=prerequisites[i][0];
+            int v=prerequisites[i][1];
             adj[u].push_back(v);
+            indegree[v]++;
         }
+        queue<int> q;
         for(int i=0;i<numCourses;i++){
-            if(!vis[i]){
-                if(detect(adj,vis,i, path)){
-                    return false;
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        int cnt=0;
+        while(!q.empty()){
+            int node=q.front();
+            cnt++;
+            q.pop();
+            for(auto it: adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
                 }
             }
         }
-        return true;
+        if(cnt==numCourses){
+            return true;
+        }
+        return false;
     }
 };
