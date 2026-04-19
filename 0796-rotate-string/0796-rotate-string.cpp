986dbf2ae2c70ae082042108b568f83239e24522
Aman Garg <152692614+Amangarg5990@users.noreply.github.com>
2026-04-20 02:48:36 +0530

    Time: 0 ms (100%), Space: 8.1 MB (61.82%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,10 +1,24 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        if(s.size()!=goal.size()){
-            return false;
+        // if(s.size()!=goal.size()){
+        //     return false;
+        // }
+        // string ne=s+s;
+        // return ne.find(goal) != string::npos;
+
+        int n=s.size();
+        if(n!=goal.size()) return false;
+        for (int i=0;i<n;i++){
+            bool match = true;
+            for (int j = 0; j < n; j++) {
+                if (s[(i + j) % n] != goal[j]) {
+                    match = false;
+                    break;
+                }
+            }
+            if (match) return true;
         }
-        string ne=s+s;
-        return ne.find(goal) != string::npos;
+        return false;
     }
 };
