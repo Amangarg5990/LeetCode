82009a9c69477a7d577b2ba91ee212b3e936d10b
Aman Garg <152692614+Amangarg5990@users.noreply.github.com>
2026-04-21 03:05:48 +0530

    Time: 0 ms (100%), Space: 8.6 MB (42.64%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.cpp
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.cpp
@@ -1,23 +1,43 @@
 class Solution {
 public:
     string sortSentence(string s) {
-        string ans="";
-        vector<pair<int,string>> g;
-        string temp="";
-        for(int i=0;i<s.size();i++){
-            if(isalpha(s[i])){
-                temp+=s[i];
+        // string ans="";
+        // vector<pair<int,string>> g;
+        // string temp="";
+        // for(int i=0;i<s.size();i++){
+        //     if(isalpha(s[i])){
+        //         temp+=s[i];
+        //     }
+        //     else if(isdigit(s[i])){
+        //         g.push_back({s[i]-'0',temp});
+        //         temp="";
+        //     }
+        // }
+        // sort(g.begin(),g.end());
+        // for(int i=0;i<g.size();i++){
+        //     ans+=g[i].second;
+        //     if(i!=g.size()-1){
+        //         ans += " ";
+        //     }
+        // }
+        // return ans;
+        vector<string> g(10);
+        string temp = "";
+        for (int i = 0; i < s.size(); i++) {
+            if (isalpha(s[i])) {
+                temp += s[i];
             }
-            else if(isdigit(s[i])){
-                g.push_back({s[i]-'0',temp});
-                temp="";
+            else if (isdigit(s[i])) {
+                int pos = s[i] - '0';
+                g[pos] = temp;
+                temp = "";
             }
         }
-        sort(g.begin(),g.end());
-        for(int i=0;i<g.size();i++){
-            ans+=g[i].second;
-            if(i!=g.size()-1){
-                ans += " ";
+        string ans = "";
+        for (int i = 1; i < g.size(); i++) {
+            if (g[i] != "") {
+                if (!ans.empty()) ans += " ";
+                ans += g[i];
             }
         }
         return ans;
