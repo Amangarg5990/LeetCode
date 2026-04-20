b049adc356a7e4f2e46c38682fcd4d0ff0ba7405
Aman Garg <152692614+Amangarg5990@users.noreply.github.com>
2026-04-20 23:02:37 +0530

    Time: 0 ms (100%), Space: 11 MB (84.35%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.cpp
+++ 1122-relative-sort-array/1122-relative-sort-array.cpp
@@ -1,27 +1,45 @@
 class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
-        map<int,int> mpp;
-        for(int i: arr1){
-            mpp[i]++;
+        // map<int,int> mpp;
+        // for(int i: arr1){
+        //     mpp[i]++;
+        // }
+        // vector<int> ans;
+        // for(int i=0;i<arr2.size();i++){
+        //     int m=mpp[arr2[i]];
+        //     while(m>0){
+        //         ans.push_back(arr2[i]);
+        //         m--;
+        //     }
+        //     mpp[arr2[i]]=0;
+        // }
+        // for(auto it: mpp){
+        //     if(it.second>0){
+        //         int m=it.second;
+        //         while(m>0){
+        //             ans.push_back(it.first);
+        //             m--;
+        //         }
+        //         mpp[it.first]=0;
+        //     }
+        // }
+        // return ans;
+        vector<int> count(1001, 0);
+        for(int x : arr1) {
+            count[x]++;
         }
         vector<int> ans;
-        for(int i=0;i<arr2.size();i++){
-            int m=mpp[arr2[i]];
-            while(m>0){
-                ans.push_back(arr2[i]);
-                m--;
+        for(int x : arr2) {
+            while(count[x] > 0) {
+                ans.push_back(x);
+                count[x]--;
             }
-            mpp[arr2[i]]=0;
         }
-        for(auto it: mpp){
-            if(it.second>0){
-                int m=it.second;
-                while(m>0){
-                    ans.push_back(it.first);
-                    m--;
-                }
-                mpp[it.first]=0;
+        for(int i = 0; i < 1001; i++) {
+            while(count[i] > 0) {
+                ans.push_back(i);
+                count[i]--;
             }
         }
         return ans;
